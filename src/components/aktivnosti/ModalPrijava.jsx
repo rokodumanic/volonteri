f7dc12169bf9791f1f1a83a5f5b5778ba1e80e2f
rokodumanic <dumanicroko@gmail.com>
2024-05-04 13:52:21 +0200

    Dodavanje prijave volontera na aktivnost i update konteksta

--- src/components/aktivnosti/ModalPrijava.jsx
+++ src/components/aktivnosti/ModalPrijava.jsx
@@ -1,43 +1,57 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AppContext from "../../kontekst";
+import { Button } from "react-bootstrap";
+import { nanoid } from 'nanoid'
 
 function ModalPrijava(){
     const data = useContext(AppContext);
-    const info = data.kontekst.aktivnosti.find(obj => obj.id === data.kontekst.selected)
+    let info = data.kontekst.aktivnosti.find(obj => obj.id === data.kontekst.selected)
+    const [volonter,setVolonter] = useState({
+      ime:"",
+      prezime:"",
+      id:""
+    })
 
     function handleInputChange(e){
         const { name, value } = e.target;
-        /* if(name==="ime"){
-            data.setKontekst({ ...data.kontekst, [name]: value });
-        } else */ if(name==="prezime"){
-            data.setKontekst({ ...data.kontekst, [name]: value});
+            setVolonter({...volonter, [name]: value });
         }
-     }
+     
+
+    function handlePrijava(){
+      setVolonter({...volonter, id: info.id+"-"+nanoid()})
+      let arr = data.kontekst;
+      arr.aktivnosti.find(obj => obj.id === data.kontekst.selected).volonteri.push({volonter});
+      console.log("DATA Before:", data.kontekst);
+      data.setKontekst({...arr});
+      console.log("DATA After:", data.kontekst);
+    }
 
     return(
       <form onSubmit={()=>console.log("JAAAAAAAYYYYYY")}>
         <div>
           <label>
-            Boja:
+            Ime:
             <input
                 type='text'
                 name='ime'
-                value={info.ime}
+                value=""
                 onChange={(e)=>handleInputChange(e)}
                 className="addInput"
                 required
             />
           </label>
           <label>
-            Marka:
+            Prezime:
             <input
                 type='text'
                 name='prezime'
-                value={info.prezime}
+                value=""
                 onChange={(e)=>handleInputChange(e)}
                 className="addInput"
             />
           </label>
+          <Button onClick={handlePrijava}>Prijavi se</Button>
         </div>
       </form>
     );
